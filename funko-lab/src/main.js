897bac1880b3597fd894b39208c5536252798787
fnascimentosilva <fabricionsilva26@gmail.com>
2023-08-27 21:31:30 +0100

    criando os cards do home e carrinho

--- funko-lab/src/main.js
+++ funko-lab/src/main.js
@@ -1,11 +1,30 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/routes'
+
+// Vuetify
+/* import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives' */
+
+// Vuex
+//import store from './store'
+
+/* const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+      defaultSet: 'mdi'
+    },
+  }) */
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app
+.use(router)
+.use(vuetify)
+.use(store)
+.mount('#app')
